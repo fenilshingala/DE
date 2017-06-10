3f3de7c885440d022a0a796e141bd09d6f5e6a3f
vinitgandhi007 <vinitgandhi007@gmail.com>
2017-06-10 14:51:13 +0530

    rebased code

--- routes.py
+++ routes.py
@@ -5,7 +5,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def welcome():
-    render_template()
+    return render_template("index.html")
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template("dashboard.html")
 
 if __name__ == '__main__':
     app.run()
