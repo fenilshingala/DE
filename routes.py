da9b21233785d0a36a2151f303d54a24015f34e6
Fenil Shingala <fenilshingala@Fenils-MacBook-Air.local>
2017-06-11 14:42:18 +0530

    added header, footer, updates, submissions

--- routes.py
+++ routes.py
@@ -11,5 +11,21 @@ def welcome():
 def dashboard():
     return render_template("dashboard.html")
 
+@app.route('/header')
+def header():
+    return render_template("header.html")
+
+@app.route('/footer')
+def footer():
+    return render_template("footer.html")
+
+@app.route('/submissions')
+def submissions():
+    return render_template("submissions.html")
+
+@app.route('/updates')
+def updates():
+    return render_template("updates.html")
+
 if __name__ == '__main__':
     app.run()
